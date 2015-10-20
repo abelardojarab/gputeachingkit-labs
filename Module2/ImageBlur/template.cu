dd662d728f690a76fc4560b0ced07b6eb9e6c03c
Abdul Dakkak <abduld@wolfram.com>
2015-10-19 20:31:47 -0500

    Add template image blur

---
+++ Module2/ImageBlur/template.cu
@@ -0,0 +1,78 @@
+#include <wb.h>
+
+#define wbCheck(stmt)                                                     \
+  do {                                                                    \
+    cudaError_t err = stmt;                                               \
+    if (err != cudaSuccess) {                                             \
+      wbLog(ERROR, "Failed to run stmt ", #stmt);                         \
+      wbLog(ERROR, "Got CUDA error ...  ", cudaGetErrorString(err));      \
+      return -1;                                                          \
+    }                                                                     \
+  } while (0)
+
+#define BLUR_SIZE 5
+
+//@@ INSERT CODE HERE
+
+int main(int argc, char *argv[]) {
+  wbArg_t args;
+  int imageWidth;
+  int imageHeight;
+  char *inputImageFile;
+  wbImage_t inputImage;
+  wbImage_t outputImage;
+  float *hostInputImageData;
+  float *hostOutputImageData;
+  float *deviceInputImageData;
+  float *deviceOutputImageData;
+
+  args = wbArg_read(argc, argv); /* parse the input arguments */
+
+  inputImageFile = wbArg_getInputFile(args, 0);
+  inputMaskFile = wbArg_getInputFile(args, 1);
+
+  inputImage = wbImport(inputImageFile);
+
+  // The input image is in grayscale, so the number of channels
+  // is 1
+  imageWidth = wbImage_getWidth(inputImage);
+  imageHeight = wbImage_getHeight(inputImage);
+
+  // Since the image is monochromatic, it only contains only one channel
+  outputImage = wbImage_new(imageWidth, imageHeight, 1);
+
+  hostInputImageData = wbImage_getData(inputImage);
+  hostOutputImageData = wbImage_getData(outputImage);
+
+  wbTime_start(GPU, "Doing GPU Computation (memory + compute)");
+
+  wbTime_start(GPU, "Doing GPU memory allocation");
+  //@@ INSERT CODE HERE
+  wbTime_stop(GPU, "Doing GPU memory allocation");
+
+  wbTime_start(Copy, "Copying data to the GPU");
+  //@@ INSERT CODE HERE
+  wbTime_stop(Copy, "Copying data to the GPU");
+
+  wbTime_start(Compute, "Doing the computation on the GPU");
+  //@@ INSERT CODE HERE
+  wbTime_stop(Compute, "Doing the computation on the GPU");
+
+  wbTime_start(Copy, "Copying data from the GPU");
+  //@@ INSERT CODE HERE
+  wbTime_stop(Copy, "Copying data from the GPU");
+
+  wbTime_stop(GPU, "Doing GPU Computation (memory + compute)");
+
+  wbSolution(args, outputImage);
+
+  cudaFree(deviceInputImageData);
+  cudaFree(deviceOutputImageData);
+  cudaFree(deviceMaskData);
+
+  free(hostMaskData);
+  wbImage_delete(outputImage);
+  wbImage_delete(inputImage);
+
+  return 0;
+}
