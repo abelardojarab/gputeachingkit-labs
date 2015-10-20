7334ff712dfaec0fa8ed49b3a12fdd285ea6ff3e
Abdul Dakkak <abduld@wolfram.com>
2015-10-19 20:16:25 -0500

    add image color to grayscale lab

---
+++ Module23/OpenACCVectorAdd/description.markdown
@@ -0,0 +1,8 @@
+---
+title: OpenACC CUDA Vector Add
+author: GPU Teaching Kit
+---
+
+# Objective
+
+Implement a vector addition using OpenACC directives.
--- Module3/sources.cmake
+++ Module3/sources.cmake
@@ -1,2 +1,2 @@
 include(${CMAKE_CURRENT_LIST_DIR}/MatrixMultiplication/sources.cmake)
-include(${CMAKE_CURRENT_LIST_DIR}/ImageColorToGrayScale/sources.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/ImageColorToGrayscale/sources.cmake)
