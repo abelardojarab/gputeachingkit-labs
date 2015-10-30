49d9df1a3453ce34b71067a30c2074d27f6778db
Abdul Dakkak <abduld@wolfram.com>
2015-10-30 11:40:54 -0500

    remove module 4

--- Module4/BasicMatrixMultiplication/sources.cmake
+++
@@ -1,5 +0,0 @@
-
-add_lab("BasicMatrixMultiplication")
-add_lab_template("BasicMatrixMultiplication" ${CMAKE_CURRENT_LIST_DIR}/template.cu)
-add_lab_solution("BasicMatrixMultiplication" ${CMAKE_CURRENT_LIST_DIR}/solution.cu)
-add_generator("BasicMatrixMultiplication" ${CMAKE_CURRENT_LIST_DIR}/dataset_generator.c)
--- Module4/TiledMatrixMultiplication/sources.cmake
+++
@@ -1,5 +0,0 @@
-
-add_lab("TiledMatrixMultiplication")
-add_lab_template("TiledMatrixMultiplication" ${CMAKE_CURRENT_LIST_DIR}/template.cu)
-add_lab_solution("TiledMatrixMultiplication" ${CMAKE_CURRENT_LIST_DIR}/solution.cu)
-add_generator("TiledMatrixMultiplication" ${CMAKE_CURRENT_LIST_DIR}/dataset_generator.c)
