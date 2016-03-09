1d883d1714001969e2c1f4ef55a3cf889eb702b0
Carl Pearson <pearson@illinois.edu>
2016-03-09 16:18:32 -0600

    Descriptions incomplete, but build into PDFs

---
+++ Module8/sources.cmake
@@ -0,0 +1,1 @@
+include(${CMAKE_CURRENT_LIST_DIR}/Convolution/sources.cmake)
--- Module9/ReductionThrust/sources.cmake
+++ Module9/ReductionThrust/sources.cmake
@@ -1,4 +1,4 @@
-add_lab("Reduction")
+add_lab("ReductionThrust")
 add_lab_template("ReductionThrust" ${CMAKE_CURRENT_LIST_DIR}/template.cu)
 add_lab_solution("ReductionThrust" ${CMAKE_CURRENT_LIST_DIR}/solution.cu)
 add_generator("ReductionThrust" ${CMAKE_CURRENT_LIST_DIR}/dataset_generator.c)
