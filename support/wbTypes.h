1dc24f1bca1f554b5264bf93a7d75f2b9bb782a9
Abdul Dakkak <abduld@wolfram.com>
2016-03-09 13:28:27 -0600

    fix build break

--- support/wbTypes.h
+++ support/wbTypes.h
@@ -11,6 +11,8 @@ typedef struct st_wbTimer_t *wbTimer_t;
 typedef struct st_wbLogEntry_t *wbLogEntry_t;
 typedef struct st_wbLogger_t *wbLogger_t;
 typedef struct st_wbArg_t wbArg_t;
+typedef struct st_wbImage_t *wbImage_t;
+typedef struct st_wbFile_t *wbFile_t;
 
 #define wbTrue true
 #define wbFalse false
