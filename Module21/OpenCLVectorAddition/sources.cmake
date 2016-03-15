20d3e195a4c092105467c39deb021a7b65fdeb58
Abdul Dakkak <abduld@wolfram.com>
2016-03-15 15:03:06 -0500

    add OpenCLVectorAddition lab

--- Module21/OpenCLVectorAddition/sources.cmake
+++ Module21/OpenCLVectorAddition/sources.cmake
@@ -1,1 +1,4 @@
+
 add_lab("OpenCLVectorAddition")
+add_lab_template("OpenCLVectorAddition" ${CMAKE_CURRENT_LIST_DIR}/template.cpp)
+add_lab_solution("OpenCLVectorAddition" ${CMAKE_CURRENT_LIST_DIR}/solution.cpp)
