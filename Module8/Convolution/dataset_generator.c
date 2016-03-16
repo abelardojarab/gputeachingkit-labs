eef7839ce1025540a1eea79cdca88ba626b27a61
Carl Pearson <pearson@illinois.edu>
2016-03-16 16:19:29 -0500

    Renmove ReductionThrust

--- Module8/Convolution/dataset_generator.c
+++ Module8/Convolution/dataset_generator.c
@@ -1,3 +1,13 @@
+#include <wbDataGenerator.h>
+
 int main(void) {
+
+  // Export images
+  wbExportKind_t kind = wbExportKind_ppm;
+
+  wbPPM_GenerateParams_t ppm_params = {100, 100, 3, 0, 255};
+  wbGenerateParams_t params = {.ppm = ppm_params};
+
+  GenerateDataset()
   return 0;
 }
--- Module9/ReductionThrust/sources.cmake
+++
@@ -1,5 +0,0 @@
-add_lab("ReductionThrust")
-add_lab_template("ReductionThrust" ${CMAKE_CURRENT_LIST_DIR}/template.cu)
-add_lab_solution("ReductionThrust" ${CMAKE_CURRENT_LIST_DIR}/solution.cu)
-add_generator("ReductionThrust" ${CMAKE_CURRENT_LIST_DIR}/dataset_generator.c)
-
