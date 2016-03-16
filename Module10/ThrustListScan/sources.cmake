e5b4ddef23c16f42e1a153d0027aad0aa51bc6de
Abdul Dakkak <abduld@wolfram.com>
2016-03-16 17:41:45 -0500

    fix source.cmake

--- Module10/ThrustListScan/sources.cmake
+++ Module10/ThrustListScan/sources.cmake
@@ -1,4 +1,4 @@
 add_lab("ThrustListScan")
 add_lab_template("ThrustListScan" ${CMAKE_CURRENT_LIST_DIR}/template.cu)
 add_lab_solution("ThrustListScan" ${CMAKE_CURRENT_LIST_DIR}/solution.cu)
-add_generator("ThrustListScan" ${CMAKE_CURRENT_LIST_DIR}/dataset_generator.c)
+add_generator("ThrustListScan" ${CMAKE_CURRENT_LIST_DIR}/dataset_generator.cpp)
--- Module22/OpenACCVectorAdd/sources.cmake
+++ Module22/OpenACCVectorAdd/sources.cmake
@@ -2,4 +2,4 @@
 add_lab("OpenACCVectorAdd")
 add_lab_template("OpenACCVectorAdd" ${CMAKE_CURRENT_LIST_DIR}/template.cpp)
 add_lab_solution("OpenACCVectorAdd" ${CMAKE_CURRENT_LIST_DIR}/solution.cpp)
-add_generator("OpenACCVectorAdd" ${CMAKE_CURRENT_LIST_DIR}/dataset_generator.c)
+add_generator("OpenACCVectorAdd" ${CMAKE_CURRENT_LIST_DIR}/dataset_generator.cpp)
