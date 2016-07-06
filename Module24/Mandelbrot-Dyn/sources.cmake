d60a38c4994dfce8113e1e3e78a796f9aa7dcfaf
Abdul Dakkak <abduld@wolfram.com>
2016-07-06 09:48:39 +0100

    renames and an attempt at fixing dyn parallism

--- Module24/Mandelbrot-Dyn/sources.cmake
+++
@@ -1,4 +0,0 @@
-add_lab("Mandelbrot-Dyn")
-add_lab_template("Mandelbrot-Dyn" ${CMAKE_CURRENT_LIST_DIR}/template.cu)
-add_lab_solution("Mandelbrot-Dyn" ${CMAKE_CURRENT_LIST_DIR}/solution.cu)
-add_generator("Mandelbrot-Dyn" ${CMAKE_CURRENT_LIST_DIR}/dataset_generator.cpp)
--- Module24/sources.cmake
+++ Module24/sources.cmake
@@ -1,3 +1,2 @@
 set(CUDA_SEPARABLE_COMPILATION ON)
-#set(LINK_LIBRARIES "${LINK_LIBRARIES} /usr/local/cuda/lib64/libcudadevrt.a")
-include(${CMAKE_CURRENT_LIST_DIR}/Mandelbrot-Dyn/sources.cmake)
+include(${CMAKE_CURRENT_LIST_DIR}/Mandelbrot_Dynamic/sources.cmake)
